26ff291dce5eacbb9cded40fa6c871c40e5bcb88
zdelv <zachdelvecchio@gmail.com>
2018-03-18 15:35:30 -0400

    Add game title parse and file reading

--- main.cpp
+++ main.cpp
@@ -1,2 +1,25 @@
-#import <std>
+#include <iostream>
+#include <fstream>
 
+using namespace std;
+
+int game_title(char* title) {
+    cout << title;
+}
+
+void open_file(char* path){
+    ifstream fileData;
+    fileData.open(path);
+    char data[100];
+    for (int i=0; i < 101; i++) {
+            fileData >> data[i];
+            cout << data[i];
+    }
+}
+
+
+int main(int argc, char* argv[]) {
+    game_title(argv[1]);
+    open_file(argv[1]);
+    return 0;
+}
