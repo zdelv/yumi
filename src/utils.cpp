e6a2eb3b0035b6f3b26751e305df211d23063a40
Zach DelVecchio <zachdelvecchio@gmail.com>
2018-03-19 10:43:09 -0400

    Added utils.cpp and extract_last_bits

---
+++ src/utils.cpp
@@ -0,0 +1,13 @@
+#include <stdint.h>
+#include <iostream>
+
+using namespace std;
+
+uint32_t extract_last_bits(uint32_t bits, uint32_t extracted_bits, int num_of_bits)
+{
+    //TODO -- Add error handling for when num_of_bits is > bits
+    unsigned bitmask;
+    bitmask = (1 << num_of_bits) - 1;
+    extracted_bits = bits & bitmask;
+    return extracted_bits;
+}
