0bd131caaaac9f79f95338a011dce382e2311a38
zdelv <zachdelvecchio@gmail.com>
2018-03-19 20:53:29 -0400

    utils.h notes

--- src/utils.h
+++ src/utils.h
@@ -1,3 +1,8 @@
 #include <stdint.h>
 
+/*  Takes a uint and extracts the last x number of bits
+    Input: uint32_t bits, uint32_t extracted_bits, int num_of_bits
+    Output: uint32_t extracted_bits
+ */
+
 uint32_t extract_last_bits(uint32_t bits, uint32_t extracted_bits, int num_of_bits);
