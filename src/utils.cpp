0a14380b423d35abfd461de3cdd124a8a7ae98b4
zdelv <zachdelvecchio@gmail.com>
2018-03-19 20:35:09 -0400

    Added utils.h

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,4 +1,4 @@
-#include <stdint.h>
+#include "utils.h"
 
 uint32_t extract_last_bits(uint32_t bits, uint32_t extracted_bits, int num_of_bits)
 {
---
+++ src/utils.h
@@ -0,0 +1,3 @@
+#include <stdint.h>
+
+uint32_t extract_last_bits(uint32_t bits, uint32_t extracted_bits, int num_of_bits);
