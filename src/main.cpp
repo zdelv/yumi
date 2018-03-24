3554a4042f15162daaa8cc03d05e3e6e68132904
zdelv <zachdelvecchio@gmail.com>
2018-03-24 15:07:22 -0400

    Refactored to use std:: instead of using namespace std

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,33 @@
-#include <iostream>
 #include <fstream>
 #include <vector>
 #include "cpu/cpu.h"
+#include "log.h"
 
-using namespace std;
 
 void game_title(char *title)
 {
-    cout << title << endl;
+    std::cout << title << std::endl;
 }
 
 void open_file(char *path)
 {
-    ifstream file(path, ios::binary);
-    vector<char> file_buf((istreambuf_iterator<char>(file)), (std::istreambuf_iterator<char>()));
+    std::ifstream file(path, std::ios::binary);
+    std::vector<char> file_buf((std::istreambuf_iterator<char>(file)), (std::istreambuf_iterator<char>()));
     for (int i = 0; i < 500; i++)
     {
-        cout << file_buf[i];
+        std::cout << file_buf[i];
     }
-    cout << endl;
+    std::cout << std::endl;
 }
 
 int main(int argc, char *argv[])
 {
+    Log::Level log_level = Log::Level::debug;
+    const char error[] = "This is an error"; 
     game_title(argv[1]);
-    vector<char> file_buf;
+    std::vector<char> file_buf;
     //open_file(argv[1]);
-    cout << "Hello World" << endl;
+    std::cout << "Hello World" << std::endl;
+    Log::LogMessage(log_level, error);
     return 0;
-}
+    }
