81d24d1e5aaf14017e32a5be173c5bd43d8e54f8
zdelv <zachdelvecchio@gmail.com>
2018-03-25 16:26:29 -0400

    Created class Loader, Public create_file_buffer and read_buffer, private vector file_buf, refactor for namespace GBA

--- src/gba/loader/load.h
+++ src/gba/loader/load.h
@@ -1,11 +1,27 @@
 #include <fstream>
 #include <vector>
 
-namespace Load {
+namespace GBA {
 
+class Loader
+{
+
+  public:
     //Create a buffer vector for the file given
     //Input: Path to file
-    //Output: Vector to buffer
-    std::vector<char> create_file_buffer(char* path); 
+    //Output: Nothing; file_buf is filled for read_buffer use
+    void create_file_buffer(char *path);
 
-}
+    //Read bytes number of bytes at location iterator_start in to output_array from the file_buf
+    //Input: int n number of bytes, int iterator_start address to start at, char output_array to write bytes to
+    //Output: Return nothing, output into output_array
+    //template<class T, size_t N>
+    void read_buffer(int bytes, uint32_t interator_start, std::vector<char> &output);
+
+  private:
+    //Vector Buffer to file
+    //Made of 1 byte chars
+    std::vector<char> file_buf;
+};
+
+} //namespace GBA
