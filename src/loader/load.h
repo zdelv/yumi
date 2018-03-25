7b4538e5f36f9a74cbae783c33e1271382400a88
zdelv <zachdelvecchio@gmail.com>
2018-03-24 22:07:05 -0400

    Misc

--- src/loader/load.h
+++ src/loader/load.h
@@ -3,7 +3,7 @@
 
 namespace Load {
 
-    //Create a file buffer vector for the file given
+    //Create a buffer vector for the file given
     //Input: Path to file
     //Output: Vector to buffer
     std::vector<char> create_file_buffer(char* path); 
