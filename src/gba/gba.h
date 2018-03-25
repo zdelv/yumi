26c4d4869e88d5b65e0f097cc0d0995d01bdc0b7
zdelv <zachdelvecchio@gmail.com>
2018-03-25 16:28:17 -0400

    Flushed out namespace GBA

--- src/gba/gba.h
+++ src/gba/gba.h
@@ -1,3 +1,17 @@
-namespace GBA {
+#include "cpu/cpu.h"
+#include "loader/load.h"
+
+// Highest level class
+// Manages all of the subclasses and provides a small interface to initialize the GBA
+//
+namespace GBA
+{
+    void run();       //TODO -- Generic run statement that means nothing
+    void load_file(char *path); //TODO -- Generic load statement that means nothing
+
     class CPU;
-};
+
+    class GFX; //TODO -- Create GFX class (Graphics)
+
+    class Loader;
+}
